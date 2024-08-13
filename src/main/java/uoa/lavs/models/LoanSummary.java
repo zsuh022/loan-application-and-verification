feb3b7f6863457c7732e1f976ec5e822c2f63252
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-13 23:14:00 +1200

    feat: add loan summary class

---
+++ src/main/java/uoa/lavs/models/LoanSummary.java
@@ -0,0 +1,15 @@
+package uoa.lavs.models;
+
+public class LoanSummary {
+
+    private Double totalInterest;
+    private Double totalCost;
+
+    public Double getTotalInterest() {
+        return totalInterest;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+}
