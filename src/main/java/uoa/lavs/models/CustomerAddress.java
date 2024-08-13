8e98e3189e69918d7c15be86ae1d057d8ac80d37
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-13 22:21:45 +1200

    feat: add customer address class

---
+++ src/main/java/uoa/lavs/models/CustomerAddress.java
@@ -0,0 +1,16 @@
+package uoa.lavs.models;
+
+public class CustomerAddress {
+
+    private String type;
+    private String line1;
+    private String line2;
+    private String suburb;
+    private String city;
+    private int postCode;
+    private String country;
+    private Boolean isPrimary;
+    private Boolean isMailing;
+
+    public CustomerAddress() {}
+}
