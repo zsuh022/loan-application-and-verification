8e796a4932c3d5f9e0c7fa228e1b4264afd328d8
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-15 00:20:50 +1200

    feat: add customer employer class

---
+++ src/main/java/uoa/lavs/models/CustomerEmployer.java
@@ -0,0 +1,18 @@
+package uoa.lavs.models;
+
+public class CustomerEmployer {
+
+    private String name;
+    private String line1;
+    private String line2;
+    private String suburb;
+    private String city;
+    private String postCode;
+    private String country;
+    private String phone;
+    private String email;
+    private String web;
+    private Boolean isOwner;
+
+    public CustomerEmployer() {}
+}
