2e147099ee81ea12380288656fe78fa60cc75695
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-15 00:05:48 +1200

    feat: add getter and setter for variables in customer address class

--- src/main/java/uoa/lavs/models/CustomerAddress.java
+++ src/main/java/uoa/lavs/models/CustomerAddress.java
@@ -13,4 +13,76 @@ public class CustomerAddress {
     private Boolean isMailing;
 
     public CustomerAddress() {}
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Boolean getIsPrimary() {
+        return isPrimary;
+    }
+
+    public void setIsPrimary(Boolean isPrimary) {
+        this.isPrimary = isPrimary;
+    }
+
+    public Boolean getIsMailing() {
+        return isMailing;
+    }
+
+    public void setIsMailing(Boolean isMailing) {
+        this.isMailing = isMailing;
+    }
 }
