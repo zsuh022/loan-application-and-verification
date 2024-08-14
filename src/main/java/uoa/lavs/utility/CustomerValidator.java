8e2fd128403c703a19fd96490179cf505e1d158f
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-15 02:30:37 +1200

    feat: add initial customer validator class

---
+++ src/main/java/uoa/lavs/utility/CustomerValidator.java
@@ -0,0 +1,38 @@
+package uoa.lavs.utility;
+
+import uoa.lavs.models.Customer;
+
+import java.util.HashMap;
+
+public class CustomerValidator {
+
+    public Customer createCustomer(HashMap<String, String> map) {
+        // TODO:
+        return new Customer();
+    }
+
+    public Boolean validateCustomer(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public Boolean validateAddress(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public Boolean validateEmail(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public Boolean validatePhone(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public Boolean validateNote(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+}
