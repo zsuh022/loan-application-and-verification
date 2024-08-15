769430e75123af18ec48fdd922e2539dac69e575
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-16 04:53:49 +1200

    feat: add initial loan factory class and loan type enum

---
+++ src/main/java/uoa/lavs/utility/LoanFactory.java
@@ -0,0 +1,14 @@
+package uoa.lavs.utility;
+
+import uoa.lavs.models.Loan;
+
+public class LoanFactory {
+
+    public Loan getLoan(LoanType loanType) {
+        // TODO:
+    }
+
+    public Boolean validateMortgage() {
+        // TODO:
+    }
+}
