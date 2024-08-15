ae5897fc786ab86de4f5620e8b758602b9e62fd2
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-16 06:38:51 +1200

    feat: implement validate phone method

--- src/main/java/uoa/lavs/utility/CustomerValidator.java
+++ src/main/java/uoa/lavs/utility/CustomerValidator.java
@@ -21,27 +21,82 @@ public class CustomerValidator {
     }
 
     public Boolean validateCustomer(HashMap<String, String> map) {
-        // TODO:
+
+        // private String customerId;
+        // private String title;
+        // private String name;
+        // private LocalDate dateOfBirth;
+        // private String occupation;
+        // private String citizenship;
+        // private String visa;
+        // private List<CustomerAddress> addressList = new ArrayList<>();
+        // private List<CustomerEmail> emailList = new ArrayList<>();
+        // private List<CustomerNote> noteList = new ArrayList<>();
+        // private List<CustomerPhone> phoneList = new ArrayList<>();
+
         return true;
     }
 
     public Boolean validateAddress(HashMap<String, String> map) {
-        // TODO:
+
+        // private String type;
+        // private String line1;
+        // private String line2;
+        // private String suburb;
+        // private String city;
+        // private int postCode;
+        // private String country;
+        // private Boolean isPrimary;
+        // private Boolean isMailing;
+
         return true;
     }
 
     public Boolean validateEmail(HashMap<String, String> map) {
-        // TODO:
+
+        // private String address;
+        // private Boolean isPrimary;
+
         return true;
     }
 
     public Boolean validatePhone(HashMap<String, String> map) {
-        // TODO:
+
+        // private String type;
+        // private String prefix;
+        // private String number;
+        // private Boolean isPrimary;
+        // private Boolean isTexting;
+
+        if (map.get("type") == null || map.get("type").isEmpty()) {
+            return false;
+        }
+
+        if (map.get("prefix") == null || map.get("prefix").matches("\\d{3}")) {
+            // prefix exactly 3 digits
+            return false;
+        }
+
+        if (map.get("number") == null || map.get("number").matches("\\d+")) {
+            // number only has digits
+            return false;
+        }
+
+        if (map.get("isPrimary") == null || map.get("isPrimary").isEmpty()) {
+            return false;
+        }
+
+        if (map.get("isTexting") == null || map.get("isTexting").isEmpty()) {
+            return false;
+        }
+
         return true;
     }
 
     public Boolean validateNote(HashMap<String, String> map) {
-        // TODO:
+
+        // private String note;
+
         return true;
     }
 }
