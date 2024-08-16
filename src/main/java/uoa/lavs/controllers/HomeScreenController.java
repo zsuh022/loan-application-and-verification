7c76fb2df91436e026ff449b4bd46817daee7047
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-17 03:27:44 +1200

    feat: add button functionality in fxml files
    note: methods todo

--- src/main/java/uoa/lavs/controllers/HomeScreenController.java
+++ src/main/java/uoa/lavs/controllers/HomeScreenController.java
@@ -8,4 +8,19 @@ public class HomeScreenController {
     @FXML private Button btnSearch;
     @FXML private Button btnAddNew;
     @FXML private Button btnDrafts;
+
+    @FXML
+    private void onSearchButtonClicked() {
+        // reveal search customer and search loan
+    }
+
+    @FXML
+    private void onAddNewButtonClicked() {
+        // reveal add customer and add loan
+    }
+
+    @FXML
+    private void onDraftsButtonClicked() {
+        // go to drafts screen
+    }
 }
