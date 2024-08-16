59cd465695a8171cb4fc34672b247b9a8c537a23
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-17 03:07:02 +1200

    feat: update initial new customer controller
    note: UML diagram

--- src/main/java/uoa/lavs/controllers/NewCustomerScreenController.java
+++ src/main/java/uoa/lavs/controllers/NewCustomerScreenController.java
@@ -3,7 +3,16 @@ package uoa.lavs.controllers;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 
+import java.util.HashMap;
+
 public class NewCustomerScreenController {
 
     @FXML private FontAwesomeIconView newCustomerBack;
+
+    private HashMap<String, String> customerValuesMap = new HashMap<>();
+
+    public boolean submitNewCustomer() {
+        // TODO:
+        return false;
+    }
 }
