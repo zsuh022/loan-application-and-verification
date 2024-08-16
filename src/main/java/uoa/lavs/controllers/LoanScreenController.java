d97e2de90a36d3c66324016ff3e1a6a97d852fdc
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-17 03:15:48 +1200

    feat: update initial loan controller
    note: UML diagram

--- src/main/java/uoa/lavs/controllers/LoanScreenController.java
+++ src/main/java/uoa/lavs/controllers/LoanScreenController.java
@@ -2,8 +2,19 @@ package uoa.lavs.controllers;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
+import uoa.lavs.models.Loan;
+
+import java.util.HashMap;
 
 public class LoanScreenController {
 
     @FXML private FontAwesomeIconView loanBack;
+
+    private Loan activeLoan;
+    private HashMap<String, String> changesMap = new HashMap<>();
+
+    public boolean submitLoanUpdate() {
+        // TODO:
+        return true;
+    }
 }
