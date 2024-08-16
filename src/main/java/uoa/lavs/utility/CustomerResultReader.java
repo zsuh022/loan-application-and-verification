8f2e150308e71932a5f0a41f78d811fa2f95db01
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-17 01:17:18 +1200

    feat: create customer search and customer result reader classes

---
+++ src/main/java/uoa/lavs/utility/CustomerResultReader.java
@@ -0,0 +1,12 @@
+package uoa.lavs.utility;
+
+import uoa.lavs.models.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerResultReader {
+
+    public boolean processSearch(ArrayList<Customer> search) {
+        // TODO:
+    }
+}
---
+++ src/main/java/uoa/lavs/utility/CustomerSearch.java
@@ -0,0 +1,12 @@
+package uoa.lavs.utility;
+
+import uoa.lavs.models.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerSearch {
+
+    public ArrayList<Customer> getSearchResults(String search) {
+        // TODO:
+    }
+}
