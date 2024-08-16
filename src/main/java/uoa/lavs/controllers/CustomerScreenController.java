3b341a1edfac3d2a4c0d2eb0724a23c751d152e7
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-17 03:09:43 +1200

    feat: update initial customer controller
    note: UML diagram

--- src/main/java/uoa/lavs/controllers/CustomerScreenController.java
+++ src/main/java/uoa/lavs/controllers/CustomerScreenController.java
@@ -2,8 +2,39 @@ package uoa.lavs.controllers;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
+import uoa.lavs.models.Customer;
+
+import java.util.HashMap;
 
 public class CustomerScreenController {
 
     @FXML private FontAwesomeIconView CustomerBack;
+
+    private Customer activeCustomer;
+    private HashMap<String, String> changeMap = new HashMap<>();
+
+    public boolean submitCustomerUpdate(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public boolean submitAddressUpdate(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public boolean submitEmailUpdate(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public boolean submitNoteUpdate(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
+
+    public boolean submitPhoneUpdate(HashMap<String, String> map) {
+        // TODO:
+        return true;
+    }
 }
