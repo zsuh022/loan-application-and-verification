5c36dc189affd8f1432d9bd6e62f44471bfc9d60
Woojin <wjeo779@aucklanduni.ac.nz>
2024-08-17 12:28:34 +1200

    rename setter

--- src/main/java/uoa/lavs/models/Customer.java
+++ src/main/java/uoa/lavs/models/Customer.java
@@ -107,7 +107,7 @@ public class Customer {
         return note;
     }
 
-    public void addNote(CustomerNote note) {
+    public void setNote(CustomerNote note) {
         this.note = note;
     }
 
