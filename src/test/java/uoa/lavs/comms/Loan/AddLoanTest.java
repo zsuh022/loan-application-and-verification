2a8501be388e844c53aff56a3c3ac63361383365
Woojin <wjeo779@aucklanduni.ac.nz>
2024-08-21 20:36:41 +1200

    Fix loading loan from mainframe + updating loan status

--- src/test/java/uoa/lavs/comms/Loan/AddLoanTest.java
+++ src/test/java/uoa/lavs/comms/Loan/AddLoanTest.java
@@ -4,35 +4,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uoa.lavs.comms.AbstractCustomerTest;
 import uoa.lavs.comms.AbstractLoanTest;
-import uoa.lavs.mainframe.Connection;
-import uoa.lavs.mainframe.MockConnection;
-import uoa.lavs.mainframe.Response;
-import uoa.lavs.mainframe.Status;
+import uoa.lavs.mainframe.*;
 import uoa.lavs.models.Customer.Customer;
 import uoa.lavs.models.Loan.Loan;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddLoanTest extends AbstractLoanTest<Loan> {
 
+    String id = null;
+
+    UpdateStatus statusUpdate = new UpdateStatus();
 
     @Override
     @BeforeEach
     protected void setup() throws IOException {
         super.setup();
-        addLoan.add(conn, loan);
+        id = addLoan.add(conn, loan);
+        loan.setLoanId(id);
+        statusUpdate.add(conn, LoanStatus.Active, id);
     }
 
     @Override
     protected void assertDetails(Loan expected, Loan actual) {
+        if (!expected.equals(actual)) {
+            fail();
+        }
     }
 
     @Test
     protected void testLoanSuccess() {
-        Loan newLoan = searchLoan.findById(conn, "1-01");
+        Loan newLoan = searchLoan.findById(conn, id);
         assertDetails(loan, newLoan);
     }
 }
