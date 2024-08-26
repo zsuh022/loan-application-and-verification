c6d02d13e78884c651a4440a39a655704d2e7f98
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-08-26 21:07:08 +1200

    feat: add tests for loan details
    note: 100% branch coverage

--- src/test/java/uoa/lavs/models/Loan/LoanDetailsTests.java
+++ src/test/java/uoa/lavs/models/Loan/LoanDetailsTests.java
@@ -1,4 +1,35 @@
 package uoa.lavs.models.Loan;
 
-public class LoanDetailsTests {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uoa.lavs.mainframe.Frequency;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoanDetailsTests {
+
+    private LoanDetails loanDetails;
+
+    @BeforeEach
+    void setUp() {
+        loanDetails = new LoanDetails();
+    }
+
+    @Test
+    void testGetFrequencyString_Weekly() {
+        loanDetails.frequency = Frequency.Weekly;
+        assertEquals("Weekly", loanDetails.getFrequencyString());
+    }
+
+    @Test
+    void testGetFrequencyString_Fortnightly() {
+        loanDetails.frequency = Frequency.Fortnightly;
+        assertEquals("Fortnightly", loanDetails.getFrequencyString());
+    }
+
+    @Test
+    void testGetFrequencyString_Monthly() {
+        loanDetails.frequency = Frequency.Monthly;
+        assertEquals("Monthly", loanDetails.getFrequencyString());
+    }
 }
